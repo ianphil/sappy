89efb9df0789074a1bb4d13b2add36cfb557b9f6
tripdubroot <ian.philpot@microsoft.com>
2017-08-05 03:16:07 -0400

    Got band data from spotify

---
+++ simple_spotify.py
@@ -0,0 +1,15 @@
+import json
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+with open("secrets.json") as secrets_file:
+    secrets = json.load(secrets_file)
+
+client_credentials_manager = SpotifyClientCredentials(secrets["SPOTIFY_CLIENT_ID"], secrets["SPOTIFY_CLIENT_SECRET"])
+spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+name = "Aerosmith"
+results = spotify.search(q='artist:' + name, type='artist')
+
+for item in results['artists']['items']:
+    print item["name"]
